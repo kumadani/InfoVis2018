c1376b869e30ab0ec87cf44ad7b2b030ffc77a4f
kumadani <kumadanikazuya@yahoo.co.jp>
2018-05-02 15:05:50 +0900

    Update w08_task02.js

--- w08/w08_task02.js
+++ w08/w08_task02.js
@@ -43,8 +43,8 @@ function main()
     {
         var S = i / 255.0; // [0,1]
         var R = Math.max( 1.0, 0.0 );
-        var G = Math.max( Math.cos( ( 1.0 - S  ) * Math.PI ), 0.0 );
-        var B = Math.max( Math.cos( ( 1.0 - S )* Math.PI ), 0.0 );
+        var G = Math.max( 1.0-S, 0.0 );
+        var B = Math.max( 1.0-S , 0.0 );
         var color = new THREE.Color( R, G, B );
         cmap.push( [ S, '0x' + color.getHexString() ] );
     }
