0ad5e71ea812102b64ed735990004f140bf4e14c
kumadani <kumadanikazuya@yahoo.co.jp>
2018-05-09 14:39:42 +0900

    Update and rename Isosurfaces.js to Isosurfaces_task01.js

--- w10/Isosurfaces_task01.js
+++ w10/Isosurfaces_task01.js
@@ -67,9 +67,9 @@ function Isosurfaces( volume, isovalue )
     for ( var i = 0; i < 256; i++ )
     {
         var S = i / 255.0; // [0,1]
-        var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
-        var G = Math.max( Math.cos( ( S - 0.5 ) * Math.PI ), 0.0 );
-        var B = Math.max( Math.cos( S * Math.PI ), 0.0 );
+        var R = Math.max( 1.0 );
+        var G = Math.max( 1.0-S , 0.0 );
+        var B = Math.max( 1.0-S , 0.0 );
         var color = new THREE.Color( R, G, B );
         cmap.push( [ S, '0x' + color.getHexString() ] );
     }
